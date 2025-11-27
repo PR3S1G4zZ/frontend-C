304d3b43a3f71d67723d93f4321dd456b09d82d1
PR3S1G4 <153407926+PR3S1G4zZ@users.noreply.github.com>
2025-11-27 16:49:05 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,14 +6,22 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Configuraci�n para producci�n/desarrollo
+// Configuración para producción/desarrollo
 var apiBase = builder.HostEnvironment.IsProduction()
     ? "https://backend-c-production.up.railway.app"  // URL de tu API en Railway
     : "http://localhost:5216";             // URL local de tu API
 
+// HttpClient para TU backend
 builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri(apiBase)
+});
+
+// HttpClient para Dog API
+builder.Services.AddHttpClient("DogAPI", client => 
+{
+    client.BaseAddress = new Uri("https://api.thedogapi.com/v1/");
+    client.DefaultRequestHeaders.Add("x-api-key", "live_IO5ZjVjwigVhC3SrfgvEMNe2fB22sSL5H998b9RAtEBkXIkPfRhEDlZQuWbKAcYz");
 });
 
 await builder.Build().RunAsync();
