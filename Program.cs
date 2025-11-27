53c9c0ef4c6186456f403fe734d720173a4af1a1
Presiga <presigadurangoesteban@gmail.com>
2025-11-27 14:30:57 -0500

    Initial commit: Blazor WASM frontend for Dog API

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using ConsumoAPI2.Wasm;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Configuraci�n para producci�n/desarrollo
+var apiBase = builder.HostEnvironment.IsProduction()
+    ? "https://backend-c-production.up.railway.appñ"  // URL de tu API en Railway
+    : "http://localhost:5216";             // URL local de tu API
+
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(apiBase)
+});
+
+await builder.Build().RunAsync();
